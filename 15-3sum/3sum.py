53a23b2a2edcd2a381b288e08a85ef12a26d9664
Sahnawaz <87632457+SahnawazShaban@users.noreply.github.com>
2023-09-11 22:55:08 +0530

    Time: 1008 ms (75.90%) | Memory: 20.4 MB (64.82%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,30 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        nums.sort()
+        result = []  # A list to store the triplets found.
+        nums.sort()  # Sort the input array.
 
-        if len(nums) < 3:
-            return []
-
+        # Iterate over the input array and fix the first element of the triplet.
         for idx, val in enumerate(nums):
-            if(idx > 0 and val == nums[idx-1]):
+
+            # Skip duplicate elements.
+            if idx > 0 and val == nums[idx - 1]:
                 continue
 
-            l = idx+1
-            r = len(nums)-1
+            # Initialize the left and right pointers.
+            l = idx + 1
+            r = len(nums) - 1
 
-            while (l<r):
+            # Keep the sum of the three elements equal to zero.
+            while l < r:
                 temp = val + nums[l] + nums[r]
 
-                if(temp < 0):
+                # If the sum is less than zero, increment the left pointer.
+                if temp < 0:
                     l += 1
-                elif(temp > 0):
+
+                # If the sum is greater than zero, decrement the right pointer.
+                elif temp > 0:
                     r -= 1
+
+                # Otherwise, the sum is equal to zero and we have found a triplet.
                 else:
-                    result.append([val,nums[l],nums[r]])
+                    # Add the triplet to the result list.
+                    result.append([val, nums[l], nums[r]])
+
+                    # Increment the left pointer and skip duplicate elements.
                     l += 1
-                    while(nums[l] == nums[l-1] and l<r):
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    
+
         return result
         
